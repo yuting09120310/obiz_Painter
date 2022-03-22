8b145cf62e4cef1555069e55ec4fa095f5486377
yuting <yuting09120310@gmail.com>
2022-03-22 16:49:53 +0800

    完成

--- Form1.cs
+++ Form1.cs
@@ -13,35 +13,39 @@ namespace obiz_Painter
 
     public partial class Form1 : Form
     {
+
+        int x0, y0;
+        Graphics Layer;
+        Pen pen;
+        Bitmap Painter;
+
         public Form1()
         {
             InitializeComponent();
             setup();
         }
 
-        int x0, y0;
-        Graphics K;
-        Pen pen;
-        Bitmap Painter;
+
 
         public void setup()
         {
             Painter = new Bitmap(775, 450);
-            K = Graphics.FromImage(Painter);
+            //創建一個可編輯的圖層
+            Layer = Graphics.FromImage(Painter);
             pen = new Pen(Color.Black, 1);
         }
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            getPoint(e.X,e.Y);
+            getPoint(e.X,e.Y);                                //抓取當前座標
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
             if(e.Button == MouseButtons.Left)
             {
-                K.DrawLine(pen, x0, y0, e.X, e.Y);
-                getPoint(e.X, e.Y);
+                K.DrawLine(pen, x0, y0, e.X, e.Y);            //
+                getPoint(e.X, e.Y);                           //抓取當前座標
                 pictureBox1.Image = Painter;
             }
         }
